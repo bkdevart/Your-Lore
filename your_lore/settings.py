0d54ee6fbfb5710b5df4b7510cda0bd24ba191f7
Brandon Knox <bknox.dev@gmail.com>
2021-04-23 20:31:09 -0700

    modified models app for lore data, tweaked settings and ran first migration

--- your_lore/settings.py
+++ your_lore/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'lore.apps.SnippetsConfig',
+    'lore.apps.LoreConfig',
 ]
 
 MIDDLEWARE = [
