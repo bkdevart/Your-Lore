eea6d18d680aa8657082433af3088a16188d9cdf
Brandon Knox <bknox.dev@gmail.com>
2021-04-23 21:42:24 -0700

    Externalized SECRET_KEY

--- your_lore/settings.py
+++ your_lore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*0b%cn7p%$$-693wm7l66ewr&wy+m5&&loebx40xl!38-&qh&f'
+# Externalized SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
