b9608c4a6fb8b39afafa8f89a8ad705091d54aad
Brandon Knox <bknox.dev@gmail.com>
2021-04-28 19:32:49 -0700

    Changed group field name to groups in Lore model to fix error

--- lore/models.py
+++ lore/models.py
@@ -4,7 +4,7 @@ class Lore(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     story = models.TextField()
-    group = models.ManyToManyField('auth.group', related_name='lore')
+    groups = models.ManyToManyField('auth.group', related_name='lore')
     owner = models.ForeignKey('auth.user', related_name='lore', on_delete=models.CASCADE)
 
     class Meta:
